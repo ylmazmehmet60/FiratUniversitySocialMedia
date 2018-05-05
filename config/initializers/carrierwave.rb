901def32bc5c9a3fb29432cfe34facc4d8b8c541
yokyokyok <170541077@firat.edu.tr>
2018-05-05 18:01:34 +0000

    gemlock22

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,12 @@
-CarrierWave.configure do |config|
-  if Rails.env.production?
+if Rails.env.production?
+  CarrierWave.configure do |config|
     config.fog_credentials = {
-      provider:              'AWS', # required
-      aws_access_key_id:     Rails.application.secrets.aws_access_key_id, # required
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
-      region:                'us-east-1', # optional, defaults to 'us-east-1'
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region                => ENV['S3_REGION']
     }
-    config.fog_directory  = 'socify' # required
-    config.fog_public     = true # optional, defaults to true
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
