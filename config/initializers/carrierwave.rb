8fdfab4d0d3d0ee63af6a04bed757290b7ed3f55
yokyokyok <170541077@firat.edu.tr>
2018-05-05 18:18:57 +0000

    gemlock22

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,8 @@ if Rails.env.production?
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :region                => ENV['S3_REGION']
+      :region                => ENV['S3_REGION'],
+      :endpoint              => 'http://s3.amazonaws.com'
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
